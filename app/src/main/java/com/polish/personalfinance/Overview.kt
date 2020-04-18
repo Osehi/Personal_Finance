99aca32127930eec6b6d61374b7ebd92c0f65df8
Osehi <osehiaseehilen@gmail.com>
2020-04-18 21:31:03 +0100

    toolbar-connectedToFragment

--- app/src/main/java/com/polish/personalfinance/Overview.kt
+++ app/src/main/java/com/polish/personalfinance/Overview.kt
@@ -1,15 +1,17 @@
 package com.polish.personalfinance
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 /**
  * A simple [Fragment] subclass.
  */
 class Overview : Fragment() {
+
+    lateinit var  oToolbar:Toolbar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,7 +20,19 @@ class Overview : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_overview, container, false)
 
+        setHasOptionsMenu(true)
+
+        oToolbar = view.findViewById(R.id.overviewToolbar)
+        (activity as AppCompatActivity).setSupportActionBar(oToolbar)
+        (activity as AppCompatActivity).supportActionBar?.title = "Overview"
+
+
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.overview_toolbar_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
 }
